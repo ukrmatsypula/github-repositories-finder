d2e15b5d42dfd46e1f175f41d31b6bedf0a3c80b
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-30 20:17:10 +0200

    init: init project

---
+++ src/components/Search.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper__search"></div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+svg {
+  color: #e4e4e4;
+}
+/* Search */
+.wrapper__search {
+  position: relative;
+}
+.search {
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  position: absolute;
+}
+.search-icon {
+  height: 100%;
+  padding-left: 16px;
+  padding-right: 16px;
+  align-items: center;
+  display: flex;
+}
+input {
+  padding: 16px 16px 16px 56px !important;
+  margin-bottom: 0px;
+  border-radius: 30px;
+  color: #909399;
+  font-size: 16px;
+  min-width: 400px;
+  height: auto;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container"></div>
+    </section>
+  </div>
+</template>
+
+<script>
+import search from "@/components/Search.vue";
+import axios from "axios";
+export default {
+  components: { search },
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+button {
+  margin-top: 40px;
+}
+.repos__wrapper {
+  width: 400px;
+  margin: 30px 0;
+}
+.repos-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding: 10px 0;
+  border-bottom: 1px solid #dbdbdb;
+}
+</style>
